6c7442ba790afc718862631201fbd0477db7b157
Benjamin Armintor <armintor@gmail.com>
2023-02-12 12:05:15 -0500

    ignore gzip archives and add usage note to collect script

--- collect.sh
+++ collect.sh
@@ -2,9 +2,10 @@
 # cd mods_for_hyacinth_import
 if [ "$1" ]; then
   cat modsCollection.head > "$1-mods.xml"
-  cat css/ldpd*.xml | grep -v "?xml version" >> "$1-mods.xml"
+  cat $1/ldpd*.xml | grep -v "?xml version" >> "$1-mods.xml"
   cat modsCollection.foot >> "$1-mods.xml"
   gzip "$1-mods.xml"
 else
-  echo "no xml directory given!"
+  echo " no xml directory given!"
+  echo "usage: ./collect.sh XML_DIR"
 fi
